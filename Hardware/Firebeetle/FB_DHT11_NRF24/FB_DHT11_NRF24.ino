7534fa5e7fa43c1b9731ac239cf2ec55787c7668
habis-git <marc.h@begger.ch>
2019-09-03 11:25:39 +0200

    Bodenfeuchtigkeits-Sensor hinzugefügt

--- Hardware/Firebeetle/FB_DHT11_NRF24/FB_DHT11_NRF24.ino
+++ Hardware/Firebeetle/FB_DHT11_NRF24/FB_DHT11_NRF24.ino
@@ -9,6 +9,7 @@
 
 // DHT Sensor
 uint8_t DHTPin = 27;
+uint8_t MOISTSENSOR_PIN = 36;
 
 /* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
 RF24 radio(25, 26); //CE, CSN
@@ -17,7 +18,8 @@ const byte address[6] = "00001";
 
 float Temperature;
 float Humidity;
-const uint8_t packetSize = sizeof(float) + 1 + sizeof(float);
+int Moisture;
+const uint8_t packetSize = sizeof(float) + 1 + sizeof(float) + 1 + sizeof(int);
 
 // Initialize DHT sensor.
 DHT dht(DHTPin, DHTTYPE);
@@ -27,16 +29,17 @@ DHT dht(DHTPin, DHTTYPE);
 // Used to control whether this node is sending or receiving
 void setup() {
   pinMode(DHTPin, INPUT);
+  pinMode(MOISTSENSOR_PIN, INPUT);
   dht.begin();
 
   Serial.begin(115200);
-  Serial.println(F("RF24/examples/GettingStarted"));
+  Serial.println(F("Temperature/Moisture measurement started"));
 
   if (radio.begin()) {
-    Serial.println("Alles Super");
+    Serial.println("Radio device ready");
   }
   else {
-    Serial.println("Nicht gut");
+    Serial.println("Radio device not ready!");
   }
   // Set the PA Level low to prevent power supply related issues since this is a
   // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
@@ -47,8 +50,10 @@ void loop() {
   Temperature = dht.readTemperature(); // Gets the values of the temperature
   Humidity = dht.readHumidity(); // Gets the values of the humidity
 
+  Moisture = analogRead(MOISTSENSOR_PIN);
+
   char packet[packetSize];
-  sprintf(packet, "%.1f;%.1f", Temperature, Humidity);
+  sprintf(packet, "%.1f;%.1f;%i", Temperature, Humidity, Moisture);
 
   Serial.println(F("Now sending: "));
   Serial.println(packet);
@@ -56,6 +61,6 @@ void loop() {
   if (!radio.write( &packet, packetSize )) {
     Serial.println(F("failed"));
   }
- // Try again 1s later
+  // Try again 1s later
   delay(5000);
 }
